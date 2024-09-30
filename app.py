c210b2b3b191598f677f2ac4b22778f5974572b1
Masachika Kamada <63488322+masachika-kamada@users.noreply.github.com>
2024-10-01 02:32:04 +0900

    [update] modelをgpt-4oのみに制限

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ import streamlit as st
 import streamlit_authenticator as stauth
 from dotenv import load_dotenv
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
-from langchain_cohere import ChatCohere
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_groq import ChatGroq
 from langchain_openai import AzureChatOpenAI
 from st_img_pastebutton import paste
 
@@ -18,19 +15,10 @@ load_dotenv(Path(__file__).parent / ".env")
 
 class LLMChatManager:
     def __init__(self):
-        self.providers = ["OpenAI", "Google", "Cohere", "Groq"]
-        self.models = {
-            # デプロイ作成時、デプロイ名とモデル名が同じになるように設定
-            "OpenAI": ["gpt-4o", "gpt-4", "gpt-35-turbo-instruct"],
-            "Google": ["gemini-1.5-flash", "gemini-1.5-pro"],
-            "Cohere": ["command-r-plus", "command-r", "command", \
-                       "command-light", "command-nightly", "command-light-nightly"],
-            "Groq": ["llama3-70b-8192", "llama3-8b-8192"],
-        }
         self.system_prompt = "あなたは愉快なAIです。ユーザの入力に日本語で答えてください"
-        self.provider = self.providers[0]
-        self.model = self.models[self.provider][0]
+        self.model = "gpt-4o"
         self.temperature = 0
+        self.llm = AzureChatOpenAI(model=self.model, temperature=self.temperature)
         self.image_url = None
         if "messages" not in st.session_state:
             self.init_messages()
@@ -38,24 +26,17 @@ class LLMChatManager:
     def select(self):
         with st.sidebar:
             st.sidebar.title("🧠 LLM Chat")
-            self._model_options()
             self._prompt_options()
             self._image_input_options()
             self.clear_conversation_button()
 
-    def _model_options(self):
-        with st.expander(f"Model Options"):
-            self.provider = st.radio("Select Provider", self.providers, on_change=self.init_messages)
-            self.model = st.selectbox("Select Model", self.models[self.provider], on_change=self.init_messages)
-            self.temperature = st.slider("Temperature", 0.0, 1.0, 0.0, 0.01)
-
     def _prompt_options(self):
-        with st.expander("Prompt Options"):
+        with st.expander("Settings, Prompt"):
             self.system_prompt = st.text_area("System Prompt", self.system_prompt, height=140)
-            self.n_history = st.slider("Number of History", 1, 10, 8, 1, help="Number of previous messages to consider")
+            self.n_history = st.slider("Number of History", 1, 14, 10, 1, help="Number of previous messages to consider")
 
     def _image_input_options(self):
-        with st.expander("Image Input Options", expanded=True):
+        with st.expander("Image Input", expanded=True):
             st.warning("Only available with GPT-4o. Changing options temporarily hides chat, but it reappears after sending a message.", icon="⚠️")
             self.use_image = st.toggle("Use Image Input", False)
 
@@ -72,15 +53,6 @@ class LLMChatManager:
 
     def init_messages(self):
         st.session_state.messages = [SystemMessage(content=self.system_prompt)]
-
-    def get_llm_instance(self):
-        llm_classes = {
-            "OpenAI": AzureChatOpenAI,
-            "Google": ChatGoogleGenerativeAI,
-            "Groq": ChatGroq,
-            "Cohere": ChatCohere
-        }
-        return llm_classes[self.provider](model=self.model, temperature=self.temperature)
 
 
 def display_chat_history():
@@ -116,11 +88,7 @@ def main():
     user_input = st.chat_input("Message...")
 
     if user_input:
-        llm = llm_chat_manager.get_llm_instance()
-
-        if llm_chat_manager.model == "gpt-4o" \
-                and llm_chat_manager.use_image \
-                and llm_chat_manager.image_url is not None:
+        if llm_chat_manager.use_image and llm_chat_manager.image_url is not None:
             st.session_state.messages.append(HumanMessage(
                 content=[
                     {"type": "text", "text": user_input},
@@ -137,7 +105,7 @@ def main():
         else:
             input_messages = [st.session_state.messages[0]] + st.session_state.messages[-llm_chat_manager.n_history:]
 
-        response = display_stream(llm.stream(input_messages))
+        response = display_stream(llm_chat_manager.llm.stream(input_messages))
         st.session_state.messages.append(AIMessage(content=response))
 
 
